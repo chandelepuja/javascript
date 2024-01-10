0610654026ba2655da0402ef8a5febcb40c7fbcb
chandelepuja <chandelepuja1@gmail.com>
2024-01-11 05:12:42 +0530

    all about for in loop
    all about for in loop

--- 08_forInLoop.js
+++ 08_forInLoop.js
@@ -1,7 +1,7 @@
 let arrayy = [11, 22, 33, 44, 55, 66, 77];
 console.log(`Traversing an array using for in loop..`);
 for (const index in arrayy) {
-   console.log(arrayy[index]);
+  console.log(arrayy[index]);
 }
 
 console.log(`========================`);
@@ -9,72 +9,78 @@ let array = [11, 22, 33, 44, 55, 66, 77];
 console.log(`Traversing an array using for in loop..`);
 let sum = 0;
 for (const index in array) {
-   console.log(array[index]);
-   sum = sum + array[index];
+  console.log(array[index]);
+  sum = sum + array[index];
 }
 console.log(`Sum of array element is : ${sum}`);
 
-// console.log(`==================Sum of odd number=========`);
-// let array1 = [11, 22, 33, 44, 55, 66, 77];
-// console.log(`Traversing an array using for in loop..`);
-// let sum1 = 0;
-// for (const index in array1) {
-//     if (array1[index]%=1) {
-//         sum1 = sum1 + array1[index];
-
-        
-//     }
-// }
-
-//    console.log(array1[index]);
-//    sum1 = sum1 + array1[index];
-
-// console.log(`Sum of array element is : ${sum1}`);
+console.log(`=====log only even number using for in loop====`);
+for (const index in array) {
+  if (array[index] % 2 == 0) {
+    console.log(array[index]);
+  }
+}
 
 console.log(`=========================================`);
+let arrayNum = [4, 5, 6, 7, 3]
+
 console.log(`Traversing an array using for of loop..`);
-for (const element of array) {
+
+for (const element of arrayNum) {
+
    console.log(element); 
 
-console.log(`************************`);
-
-let arrayNumm = [4, 5, 6, 7, 3]
-console.log(`Traversing an array using for of loop..`);
-let result = 0;
-for (const element of arrayNumm) {
-    result = result + arrayNumm[element];
-   console.log(element); 
 }
-console.log(result);
+  let arrayNumm = [4, 5, 6, 7, 3];
+  console.log(`Traversing an array using for of loop..`);
+  let result = 0;
+  for (const element of arrayNumm) {
+    console.log(element);
+    result = result + element;
+  }
+  console.log(`Sum of element present in array is = ${result}`);
+  console.log(`=================================================`);
 
-
-console.log(`==== WAP to count the vowels from the given string=======`);
-const str = "Developer UI and Backend";
-const vowels = "aeiou";
-let count = 0;
-for (const char of str) {
+  console.log(`==== WAP to count the vowels from the given string=======`);
+  const str = "Developer UI and Backend";
+  const vowels = "aeiou";
+  let count = 0;
+  for (const char of str) {
     console.log(char);
     if (vowels.includes(char.toLowerCase())) {
-        count++;
+      count++;
     }
-}
-console.log(`Vowels count is: ${count}`);
+  }
+  console.log(`Vowels count is: ${count}`);
 
-console.log(`*********join method**********`);
-let arrayOfFriends = ["Jenny", "Elon", "Bill"];
-const resultt = arrayOfFriends.join(",");
-console.log(typeof resultt);
-console.log(resultt);
+  console.log(`*********join method**********`);
 
-arrayOfFriends.reverse();
-console.log(arrayOfFriends);
+  let arrayOfFriends = ["Jenny", "Elon", "Bill"];
+  const resultt = arrayOfFriends.join(",");
+  console.log(`Type of resultt is ==>`,typeof resultt);
+  console.log(`After join method we get = ${resultt}`);
+  console.log(``);
 
-const word = "Developer UI and Backend";
-const arrayOfWords = word.split(" ");
-console.log(arrayOfWords);
-console.log(arrayOfWords.length);
+  console.log(`=========Reverse method========`);
 
-const reverseStr = word.split("").reverse().join("");
-console.log(reverseStr);
+  arrayOfFriends.reverse();
+  console.log(arrayOfFriends);
+  console.log(``);
 
-}
+  console.log(``);
+  const word = "Developer UI and Backend";
+  const arrayOfWords = word.split(" ");
+  console.log(`1) After using split method is = `,arrayOfWords);
+
+  console.log(`2) Total number of words are = `,arrayOfWords.length);
+  console.log(``);
+  const words = word.split("");
+  console.log(`===========After split Array is=======`);
+  console.log(words);
+  console.log(`==============Reverse Array=============`);
+  words.reverse();
+  console.log(words);
+  console.log(`============reverse string=======`);
+
+  const reverseStr = word.split("").reverse().join("");
+  console.log(reverseStr);
