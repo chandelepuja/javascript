a947684d533fefd861ef6f81bed4f7572eeeafdc
chandelepuja <chandelepuja1@gmail.com>
2024-01-12 00:53:55 +0530

    object merge and clone
    object merge and clone

--- 09_objectCloneAssig.js
+++ 09_objectCloneAssig.js
@@ -1,4 +1,4 @@
-console.log(`========STEP 1=========`);
+console.log(`STEP 1===Create the object 'bankSbi' using literals with 4 properties===`);
 let bankSbi = {
 bankName: "SBI",
 accountNumber: 34175362312,
@@ -7,14 +7,14 @@ manegerName:"Darshan Chandele",
 }
 console.table(bankSbi);
 
-console.log(`========STEP 2=========`);
+console.log(`STEP 2===Create the object 'bankLocation' using literals======`);
 let bankLocation = {
     street: "Keshaw Nagar",
     city: "Pune",
     pin: 411004,
 }
 console.table(bankLocation);
-console.log(`========STEP 3=========`);
+console.log(`STEP 3===Clone STEP 1 and STEP 2 using 'object.assign()' method===`);
 console.log(`***  Clone bankSbi ***`);
 let clone1 = Object.assign({},bankSbi)
 console.log(`After clone in new object we change the bank name :`);
@@ -26,21 +26,21 @@ clone2.pin = 400114;
 console.log(`After clone in new object we change the pin :`);
 console.table(clone2);
 
-console.log(`========STEP 4=========`);
+console.log(`STEP 4===Create the object 'rateOfInterest' using literals======`);
 let rateOfInterest = {
     homeLoanInterest: 7.2,
     personalLoanInterest:5.2,
     dueInterest: 3
 }
 console.table(rateOfInterest);
-console.log(`========STEP 5=========`);
-let mergeObj123 = Object.assign({}, bankSbi, bankLocation, rateOfInterest)
-console.table(mergeObj123);
+console.log(`STEP 5===Merge STEP 1, STEP 2, and STEP 4 objects into new object 'sbiDetails'===`);
+let sbiDetails = Object.assign({}, bankSbi, bankLocation, rateOfInterest)
+console.table(sbiDetails);
 
-console.log(`========STEP 6=========`);
-for (const key in mergeObj123) {
-    if (Object.hasOwnProperty.call(mergeObj123, key)) {
-        const element = mergeObj123[key];
+console.log(`STEP 6===Traverse this merged object STEP 5 using for in loop======`);
+for (const key in sbiDetails) {
+    if (Object.hasOwnProperty.call(sbiDetails, key)) {
+        const element = sbiDetails[key];
         console.log(`Key ==> ${key}, Value ==> ${element}`);
     }
 }
